daadd26421bfd1ce4dbfa4f30e203fda732ff649
Ekaterina Bogorodskaya <katyafleur@yandex.ru>
2026-01-22 18:34:30 +0300

    Первое задание по решению уравнений и ипотечному калькулятору готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	const monthlyRate = percent / 100 / 12;
+	const bodyCredit = amount - contribution;
+
+	const totalSumm = countMonths * (bodyCredit * (monthlyRate + (monthlyRate / (((1 + monthlyRate) ** countMonths) - 1))));
+
+	return +totalSumm.toFixed(2);
+
 }
