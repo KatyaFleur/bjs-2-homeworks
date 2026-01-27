ebe12122c6d393c9188fc9378b366c5a42a26b66
Ekaterina Bogorodskaya <katyafleur@yandex.ru>
2026-01-27 17:59:10 +0300

    Выполнено задание с массивами

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+return arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).reduce((acc, user, index, arr) => {
+  acc += user.age; {
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    } else {
+      return acc;
+    }
+  }
+  }, 0)
+  return result;
 }
