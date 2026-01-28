28205fbc67a7b9975e157c94fac60837573e3ec4
Ekaterina Bogorodskaya <katyafleur@yandex.ru>
2026-01-28 11:21:50 +0300

    Решена задача по объектам, функции конструкторы

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
+let student3 = new Student("Елена", "женский", 27);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		this.marks.push(...marks);
+	} else {
+		console.log("Студент отчислен, добавить оценки нельзя.");
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
